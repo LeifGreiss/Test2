6582faff116314c1fe1b7285bc5a682a3e91c981
Leif Greiss <helter1234@gmail.com>
2017-12-01 16:48:47 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 import scraperwiki
 html = scraperwiki.scrape('http://inmo.ie/6022')
-print "Click on the ...more link to see the whole page"
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
