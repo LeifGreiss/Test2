f4eb9bbd2e3c38d1845504c3b27296f8d427b560
Leif Greiss <helter1234@gmail.com>
2017-12-01 13:32:18 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import scraperwiki
-html = scraperwiki.scrape('http://www.maidencreek.net/')
-print html
+#html = scraperwiki.scrape('http://www.maidencreek.net/')
+#print html
 x = ['Carol', 'Rui', 'Brian', 'Fatimah']
 print x
 food = {"ham" : "yes", "egg" : "yes", "spam" : "no" }
