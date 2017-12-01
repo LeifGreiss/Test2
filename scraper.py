68ef52259da347e9df5bada08682fd143e93f57b
Leif Greiss <helter1234@gmail.com>
2017-12-01 13:22:12 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import scraperwiki
-clown = scraperwiki.scrape('https://www.opensecrets.org/resources/ftm/ch09p1.php')
+clown = scraperwiki.scrape('https://www.opensecrets.org')
 print clown
 sausages = clown
 print sausages
