7c88eb00b898d0ef09d9d4438dc702f28f4241ad
Leif Greiss <helter1234@gmail.com>
2017-12-01 13:26:15 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import scraperwiki
-html = scraperwiki.scrape('https://www.w3schools.com')
+html = scraperwiki.scrape('https://www.warnerbros.com/archive/spacejam/movie/jam.htm')
 print html
 sausages = html
 print sausages
+top = 2
+print top
