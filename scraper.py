d7b629f6c6f0147387e16c16ca95c3783e1ac0cc
Leif Greiss <helter1234@gmail.com>
2017-12-01 17:30:30 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,13 @@
 import scraperwiki
-html = scraperwiki.scrape('http://inmo.ie/6022')
+html = scraperwiki.scrape('https://en.wikipedia.org/wiki/List_of_British_racecourses')
 import lxml.html 
 root = lxml.html.fromstring(html)#turnourHTMLintoanlxmlobject 
-trs = root.cssselect('tr')#getallthe<tdtags
-for tr in trs:
-    print tr.text_content()
-for tr in trs:
-    record = { "tr" : tr.text_content() } # column name and value try:
-    scraperwiki.sqlite.save(["tr"], record)
+tds = root.cssselect('td')#getallthe<tdtags
+for td in tds:
+    print td.text_content()
+for td in tds:
+    record = { "td" : td.text_content() } # column name and value try:
+    scraperwiki.sqlite.save(["td"], record)
     #try:
         #scraperwiki.sqlite.save(["td"], record) # save the records one by one 
     #except:
