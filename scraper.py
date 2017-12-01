d5b5455a25008f35fadfba977e8ccb44f75f6c50
Leif Greiss <helter1234@gmail.com>
2017-12-01 13:45:21 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,6 +9,6 @@ tds = root.cssselect('td') # get all the <td> tags
 for td in tds:
     print lxml.html.tostring(td) # the full HTML tag
     print td.text                # just the text inside the HTML tag
-or td in tds:
+for td in tds:
     record = { "td" : td.text } # column name and value
     scraperwiki.sqlite.save(["td"], record) # save the records one by one
